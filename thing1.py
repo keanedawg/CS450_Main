44d31f304faadaa4b39d1fee80e168034523ebfd
keanedawg <cameronfife@hotmail.com>
2018-09-22 21:37:42 -0600

    train test split works (part 2)

--- thing1.py
+++ thing1.py
@@ -1,4 +1,7 @@
 from sklearn import datasets
+import numpy as np
+from sklearn.model_selection import train_test_split
+
 iris = datasets.load_iris()
 
 # Show the data (the attributes of each instance)
@@ -9,3 +12,13 @@ print(iris.target)
 
 # Show the actual target names that correspond to each number
 print(iris.target_names)
+
+
+#, y = np.arange(10).reshape((5, 2)), range(5)
+X_train, X_test, y_train, y_test = train_test_split(
+    iris.data, iris.target, test_size=0.3, random_state=42)
+
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
