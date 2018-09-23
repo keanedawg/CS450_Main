fe56808ca32b08b426e5d0ed85d9ab2fb46fdd14
keanedawg <cameronfife@hotmail.com>
2018-09-22 21:47:55 -0600

    applied GaussianNB to train and predict and print results

--- thing1.py
+++ thing1.py
@@ -6,25 +6,30 @@ from sklearn.naive_bayes import GaussianNB
 iris = datasets.load_iris()
 
 # Show the data (the attributes of each instance)
-print(iris.data)
+#print(iris.data)
 
 # Show the target values (in numeric format) of each instance
-print(iris.target)
+#print(iris.target)
 
 # Show the actual target names that correspond to each number
-print(iris.target_names)
+#print(iris.target_names)
 
 
 #, y = np.arange(10).reshape((5, 2)), range(5)
 data_train, data_test, target_train, target_test = train_test_split(
     iris.data, iris.target, test_size=0.3, random_state=42)
 
-print(data_train)
-print(data_test)
-print(target_train)
-print(target_test)
+#print(data_train)
+#print(data_test)
+#print(target_train)
+#print(target_test)
 
 classifier = GaussianNB()
 model = classifier.fit(data_train, target_train)
 
-print(model)
+targets_predicted = model.predict(data_test)
+print(targets_predicted)
+print(target_test)
+
+error = 1.0 - np.mean( target_test != targets_predicted )
+print(error)
