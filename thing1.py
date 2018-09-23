dd53900d45a1c9ef855c75e7cd86f0d26a200cf0
keanedawg <cameronfife@hotmail.com>
2018-09-22 21:41:39 -0600

    changed some variable names

--- thing1.py
+++ thing1.py
@@ -1,6 +1,7 @@
 from sklearn import datasets
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 
 iris = datasets.load_iris()
 
@@ -15,10 +16,15 @@ print(iris.target_names)
 
 
 #, y = np.arange(10).reshape((5, 2)), range(5)
-X_train, X_test, y_train, y_test = train_test_split(
+data_train, data_test, target_train, target_test = train_test_split(
     iris.data, iris.target, test_size=0.3, random_state=42)
 
-print(X_train)
-print(X_test)
-print(y_train)
-print(y_test)
+print(data_train)
+print(data_test)
+print(target_train)
+print(target_test)
+
+classifier = GaussianNB()
+model = classifier.fit(data_train, target_train)
+
+print(model)
