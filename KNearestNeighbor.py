a96cfcf364a71d4b849e40979d7e1e597d2fbb3e
keanedawg <cameronfife@hotmail.com>
2018-09-30 17:54:39 -0600

    import KDTree

--- KNearestNeighbor.py
+++ KNearestNeighbor.py
@@ -2,6 +2,7 @@ from sklearn import datasets
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KDTree
 from sklearn.neighbors import KNeighborsClassifier
 
 # Get iris data from somewhere
