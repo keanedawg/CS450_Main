aee3bc99082aac0ce837332a80739c26b98862a6
keanedawg <cameronfife@hotmail.com>
2018-09-30 11:45:28 -0600

    Updated template with more and better comments

--- template.py
+++ template.py
@@ -3,17 +3,22 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
+# Get iris data from somewhere
 iris = datasets.load_iris()
 
-
-class hardCodedModel:
+# Our trained machine learning model is actually a class.
+# Thus, we can create multiple models trained with different sets
+class NearestNeighborModel:
+    # Given an array, uses our algorithm to predict the result of each element
     def predict(self, test_arr):
         return [0] * len(test_arr)
-class hard:
+class NearestNeighbor:
+    # Gives array of input and output to help train the model
     def fit(self, data_train, target_train):
-        return hardCodedModel()
+        return NearestNeighborModel()
 
-
+#################################################
+# USE THESE VALUES TO SHOWCASE DATA
 # Show the data (the attributes of each instance)
 #print(iris.data)
 
@@ -21,28 +26,25 @@ class hard:
 #print(iris.target)
 
 # Show the actual target names that correspond to each number
-print(iris.target_names)
+#print(iris.target_names)
+#################################################
 
-#, y = np.arange(10).reshape((5, 2)), range(5)
+# Splits the data randomly
 data_train, data_test, target_train, target_test = train_test_split(
     iris.data, iris.target, test_size=0.3, random_state=42)
 
-#print(data_train)
-#print(data_test)
-#print(target_train)
-#print(target_test)
-
-
-
 # Comment and Uncomment to switch to Gaussian algorithm.
 #classifier = GaussianNB()
-classifier = hard()
+classifier = NearestNeighbor()
 
+# Calls the function to train the data
 model = classifier.fit(data_train, target_train)
 
+# Print amount incorrectly guessed
 targets_predicted = model.predict(data_test)
 print(targets_predicted)
 print(target_test)
 
+# Print percentage correctly guessed
 error = 1.0 - np.mean( target_test != targets_predicted )
 print(error)
